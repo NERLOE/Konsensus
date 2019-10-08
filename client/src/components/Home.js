68a56a7b2f5fd025a08fc4e9bc32b4fb4d25a5d8
NERLOE <marcusnerloe@gmail.com>
2019-10-09 00:44:24 +0200

    Update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,98 +2,105 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export class Home extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			gameCode: "",
-			error: false
-		};
-	}
+    this.state = {
+      gameCode: "",
+      error: false,
+      popupMessage: null
+    };
+  }
 
-	handleKeyPress = e => {
-		if (e.charCode == 13) {
-			// Trykkede ENTER
-			console.log("Enter");
-			this.joinGame();
-		}
-	};
+  handleKeyPress = e => {
+    if (e.charCode == 13) {
+      // Trykkede ENTER
+      console.log("Enter");
+      this.joinGame();
+    }
+  };
 
-	handleChange = e => {
-		this.setState({ gameCode: e.target.value, error: false });
-	};
+  handleChange = e => {
+    this.setState({ gameCode: e.target.value, error: false });
+  };
 
-	joinGame = () => {
-		var code = this.state.gameCode.trim();
-		if (code == "") {
-			this.setState({ error: true });
-			return;
-		}
+  joinGame = () => {
+    var code = this.state.gameCode.trim();
+    if (code == "") {
+      this.setState({ error: true });
+      return;
+    }
 
-		console.log("Trying to join game with code: " + code);
+    console.log("Trying to join game with code: " + code);
 
-		axios.get("/api/getGame/" + code).then(res => {
-			if (res.data.error) {
-				// Fejl
-				this.setState({ error: true });
-				console.log(res.data.error);
-			} else {
-				// Success
-				console.log(res.data);
-			}
-		});
-	};
+    axios.get("/api/getGame/" + code).then(res => {
+      if (res.data.error) {
+        // Fejl
+        this.setState({ error: true });
+        console.log(res.data.error);
+      } else {
+        // Success
+        console.log(res.data);
+        var game = res.data;
+        this.props.history.push("/game/" + game.id);
+      }
+    });
+  };
 
-	createGame = () => {
-		console.log("Trying to create game");
+  createGame = () => {
+    console.log("Trying to create game");
 
-		axios.put("/api/createGame").then(res => {
-			if (res.data.error) {
-				console.log(res.data.error);
-			} else {
-				console.log(res.data);
-			}
-		});
-	};
+    axios.put("/api/createGame").then(res => {
+      if (res.data.error) {
+        console.log(res.data.error);
+      } else {
+        console.log(res.data);
+        var game = res.data;
+        this.props.history.push("/game/" + game.id);
+      }
+    });
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				<h1 style={{ textAlign: "center" }}>
-					Indtast koden til dit spil, eller opret dit eget.
-				</h1>
-				<div className="homeScreen">
-					<div className="input-group input-group-lg">
-						<input
-							type="text"
-							className={
-								"form-control gameCodeInput" +
-								(this.state.error ? " shake animated wrongCode" : "")
-							}
-							placeholder="Indtast spilkode"
-							onKeyPress={this.handleKeyPress}
-							onChange={this.handleChange.bind(this)}
-							value={this.state.gameCode}
-						/>
-					</div>
-					<button
-						onClick={this.joinGame}
-						className="btn btn-lg btn-primary joinGameBtn"
-					>
-						Tilslut til spil
-					</button>
-					<h1>eller</h1>
-					<button
-						onClick={this.createGame}
-						className="btn btn-lg btn-secondary createGameBtn"
-					>
-						Opret spil
-					</button>
-				</div>
-				<div className="popupMessage"></div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <h1 style={{ textAlign: "center" }}>
+          Indtast koden til dit spil, eller opret dit eget.
+        </h1>
+        <div className="homeScreen">
+          <div className="input-group input-group-lg">
+            <input
+              type="text"
+              className={
+                "form-control gameCodeInput" +
+                (this.state.error ? " shake animated wrongCode" : "")
+              }
+              placeholder="Indtast spilkode"
+              onKeyPress={this.handleKeyPress}
+              onChange={this.handleChange.bind(this)}
+              value={this.state.gameCode}
+            />
+          </div>
+          <button
+            onClick={this.joinGame}
+            className="btn btn-lg btn-primary joinGameBtn"
+          >
+            Tilslut til spil
+          </button>
+          <h1>eller</h1>
+          <button
+            onClick={this.createGame}
+            className="btn btn-lg btn-secondary createGameBtn"
+          >
+            Opret spil
+          </button>
+        </div>
+        {this.state.popupMessage ? (
+          <div className="popupMessage">{this.state.popupMessage}</div>
+        ) : null}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;
