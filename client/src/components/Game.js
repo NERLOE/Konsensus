9afb9af6d5d43b9c75aaf673484a9e8c4817c9bb
NERLOE <marcusnerloe@gmail.com>
2019-10-09 00:50:54 +0200

    Added update for no access to non existing games

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import QRCodes from "./QRCodes";
 import QRReader from "./QRReader";
+import axios from "axios";
 
 export class Game extends Component {
+  async constructor() {
+    super();
+
+    var res = await axios.get("/api/getGame/" + code);
+
+    if (res.data.error) {
+      this.props.history.push("/");
+      return;
+    }
+
+    var g = res.data;
+    this.state = {
+      game: g
+    };
+  }
+
+  componentWillMount() {}
+
   render() {
     return (
       <React.Fragment>
