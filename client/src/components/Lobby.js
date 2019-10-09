7c48c4b57f8057ec129491276736d8e92fb2072d
Marcus <marcusnerloe@gmail.com>
2019-10-09 16:44:25 +0200

    Added delete on quit

--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -3,48 +3,57 @@ import axios from "axios";
 import { QR } from "./QR";
 
 export class Lobby extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      game: null
-    };
-  }
+		this.state = {
+			game: null
+		};
+	}
 
-  async check() {
-    console.log(this.props);
-    console.log(this.props.match.params);
-    var res = await axios.get("/api/getGame/" + this.props.match.params.gameID);
+	async check() {
+		console.log(this.props);
+		console.log(this.props.match.params);
+		var res = await axios.get("/api/getGame/" + this.props.match.params.gameID);
 
-    if (res.data.error) {
-      this.props.history.push("/");
-      return;
-    }
+		if (res.data.error) {
+			this.props.history.push("/");
+			return;
+		}
 
-    var g = res.data;
-    this.setState({ game: g });
-  }
+		var g = res.data;
+		this.setState({ game: g });
+	}
 
-  componentDidMount() {
-    this.check();
-  }
+	componentDidMount() {
+		this.check();
+	}
 
-  render() {
-    if (this.state.game) {
-      return (
-        <React.Fragment>
-          <h1>Kode:</h1>
-          <QR value={this.state.game.id}></QR>
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <React.Fragment>
-          <h1>Loader...</h1>
-        </React.Fragment>
-      );
-    }
-  }
+	componentWillUnmount() {
+		axios
+			.delete("/api/deleteGame/" + this.props.match.params.gameID)
+			.then(res => {
+				console.log(res);
+			});
+	}
+
+	render() {
+		if (this.state.game) {
+			return (
+				<React.Fragment>
+					<div className="codeInfo">
+						<QR value={this.state.game.id}></QR>
+					</div>
+				</React.Fragment>
+			);
+		} else {
+			return (
+				<React.Fragment>
+					<h1>Loader...</h1>
+				</React.Fragment>
+			);
+		}
+	}
 }
 
 export default Lobby;
--- index.js
+++ index.js
@@ -11,49 +11,75 @@ var games = [];
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.put("/api/createGame", (req, res) => {
-  var randomString = Math.random()
-    .toString(36)
-    .slice(-gameCodeLength)
-    .toUpperCase();
+	var randomString = Math.random()
+		.toString(36)
+		.slice(-gameCodeLength)
+		.toUpperCase();
 
-  var game = {
-    id: randomString,
-    players: [],
-    time: 60 * 15,
-    started: false
-  };
+	var game = {
+		id: randomString,
+		players: [],
+		time: 60 * 15,
+		started: false
+	};
 
-  games.push(game);
-  res.json(game);
+	games.push(game);
+	res.json(game);
+});
+
+app.delete("/api/deleteGame/:id", (req, res) => {
+	const id = req.params.id;
+	if (!id) {
+		res.json({ error: "ID er ikke defineret." });
+		return;
+	}
+
+	var game = null;
+	games.forEach(g => {
+		if (id == g.id) {
+			game = g;
+		}
+	});
+
+	if (game == null) {
+		res.json({ error: "Spillet med dette ID findes ikke!" });
+		return;
+	}
+
+	games = games.filter(function(value, index, arr) {
+		return value.id != id;
+	});
+
+	res.json({ success: "Spillet blev slettet!" });
 });
 
 app.get("/api/getGame/:id", (req, res) => {
-  const id = req.params.id;
-  if (!id) {
-    res.json({ error: "ID er ikke defineret." });
-    return;
-  }
+	const id = req.params.id;
+	if (!id) {
+		res.json({ error: "ID er ikke defineret." });
+		return;
+	}
 
-  var game = { error: "Spillet med dette ID findes ikke!" };
-  games.forEach(g => {
-    if (id == g.id) {
-      game = g;
-    }
-  });
+	var game = { error: "Spillet med dette ID findes ikke!" };
+	games.forEach(g => {
+		if (id == g.id) {
+			game = g;
+		}
+	});
 
-  res.json(game);
+	res.json(game);
 });
 
 app.get("/api/getGames", (req, res) => {
-  res.json(games);
+	res.json(games);
 });
 
 // Alle forespørgelser, som ikke er blevet brugt ovenover, bliver sendt til React.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+	res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 80;
 app.listen(port, () => {
-  console.log(`Serveren åbnede på porten: ${port}`);
+	console.log(`Serveren åbnede på porten: ${port}`);
 });
