ec0e8438086e9eaed98809dbb17062cfbb369413
NERLOE <marcusnerloe@gmail.com>
2019-10-09 13:54:52 +0200

    Bug fix

--- client/src/components/QRModal.js
+++ client/src/components/QRModal.js
@@ -1,57 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class QRModal extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <React.Fragment>
         <button
           type="button"
-          class="btn btn-primary"
+          className="btn btn-primary"
           data-toggle="modal"
           data-target="#exampleModalCenter"
         >
-            Scan kode
+          Scan kode
         </button>
         <div
-          class="modal fade"
+          className="modal fade"
           id="exampleModalCenter"
           tabindex="-1"
           role="dialog"
           aria-labelledby="exampleModalCenterTitle"
           aria-hidden="true"
         >
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalCenterTitle">
                   Modal title
                 </h5>
                 <button
                   type="button"
-                  class="close"
+                  className="close"
                   data-dismiss="modal"
                   aria-label="Close"
                 >
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div class="modal-body">...</div>
-              <div class="modal-footer">
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
                 <button
                   type="button"
-                  class="btn btn-secondary"
+                  className="btn btn-secondary"
                   data-dismiss="modal"
                 >
                   Close
                 </button>
-                <button type="button" class="btn btn-primary">
+                <button type="button" className="btn btn-primary">
                   Save changes
                 </button>
               </div>
             </div>
           </div>
         </div>
-        )
-    }
+      </React.Fragment>
+    );
+  }
 }
 
-export default QRModal
+export default QRModal;
